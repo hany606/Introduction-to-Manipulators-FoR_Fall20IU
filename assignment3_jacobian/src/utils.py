[Assignment3-Jacobian] Fix the bugs in Numberical derivative method
- Fix the bugs in differentiation matrices
- Successfully implement skew theory method and numerical derivative method

assignment3_jacobian/src/utils.py
@@ -5,76 +5,76 @@ def translation_x(l):
     return np.array([[1,0,0, l],
                      [0,1,0, 0],
                      [0,0,1, 0],
-                     [0,0,0, 1]])
+                     [0,0,0, 1]], dtype='float')
 
 def translation_y(l):
     return np.array([[1,0,0, 0],
                      [0,1,0, l],
                      [0,0,1, 0],
-                     [0,0,0, 1]])
+                     [0,0,0, 1]], dtype='float')
 
 def translation_z(l):
     return np.array([[1,0,0, 0],
                      [0,1,0, 0],
                      [0,0,1, l],
-                     [0,0,0, 1]])
+                     [0,0,0, 1]], dtype='float')
 
 def rotation_x(theta):
     return np.array([[1,         0,          0, 0],
                      [0,np.cos(theta),-np.sin(theta), 0],
                      [0,np.sin(theta), np.cos(theta), 0],
-                     [0,         0,          0, 1]])
+                     [0,         0,          0, 1]], dtype='float')
 
 
 def rotation_y(theta):
     return np.array([[np.cos(theta) ,0,np.sin(theta), 0],
                      [0          ,1,         0, 0],
                      [-np.sin(theta),0,np.cos(theta), 0],
-                     [0          ,0,         0, 1]])
+                     [0          ,0,         0, 1]], dtype='float')
 
 def rotation_z(theta):
     return np.array([[np.cos(theta),-np.sin(theta),0, 0],
                      [np.sin(theta), np.cos(theta),0, 0],
                      [0         ,0          ,1, 0],
-                     [0         ,0          ,0, 1]])
+                     [0         ,0          ,0, 1]], dtype='float')
 
 
 def dtranslation_x(l):
     return np.array([[1,0,0, 1],
                      [0,1,0, 0],
                      [0,0,1, 0],
-                     [0,0,0, 1]])
+                     [0,0,0, 1]], dtype='float')
 
 def dtranslation_y(l):
     return np.array([[1,0,0, 0],
                      [0,1,0, 1],
                      [0,0,1, 0],
-                     [0,0,0, 1]])
+                     [0,0,0, 1]], dtype='float')
 
 def dtranslation_z(l):
     return np.array([[1,0,0, 0],
                      [0,1,0, 0],
                      [0,0,1, 1],
-                     [0,0,0, 1]])
+                     [0,0,0, 1]], dtype='float')
 
 def drotation_x(theta):
-    return np.array([[1,         0,          0, 0],
+    return np.array([[0,         0,          0, 0],
                      [0,-np.sin(theta), -np.cos(theta), 0],
                      [0, np.cos(theta), -np.sin(theta), 0],
-                     [0,         0,          0, 1]])
+                     [0,         0,          0, 0]], dtype='float')
 
 
 def drotation_y(theta):
     return np.array([[-np.sin(theta), 0,  np.cos(theta), 0],
-                     [0          ,1,         0, 0],
+                     [0          ,0,         0, 0],
                      [-np.cos(theta), 0, -np.sin(theta), 0],
-                     [0          ,0,         0, 1]])
+                     [0          ,0,         0, 0]], dtype='float')
 
 def drotation_z(theta):
     return np.array([[-np.sin(theta),-np.cos(theta),0, 0],
                      [ np.cos(theta), -np.sin(theta),0, 0],
-                     [0         ,0          ,1, 0],
-                     [0         ,0          ,0, 1]])
+                     [0         ,0          ,0, 0],
+                     [0         ,0          ,0, 0]], dtype='float')
 
 
 def get_rotation(H):