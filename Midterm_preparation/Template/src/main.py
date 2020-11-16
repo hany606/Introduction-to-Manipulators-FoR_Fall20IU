045424aeaf058b8e997d3489bf537869754c67b6
hany606 <h.hamed.elanwar@gmail.com>
2020-11-16 17:44:11 +0300

    [Midterm exam] Add template for the exam
    - Include Jacobian, FK, IK

---
+++ Midterm_preparation/Template/src/main.py
@@ -0,0 +1,91 @@
+from robot import KUKA_KR10_R1100_2
+import numpy as np
+from utils import calc_error, get_position, get_rotation
+
+robot = KUKA_KR10_R1100_2()
+
+def calc_jacobian(q, debug=True):
+    skew = robot.jacobian(q, method="skew")
+    numerical = robot.jacobian(q, method="numerical")
+    err = calc_error(skew, numerical)
+    robot.check_singularity(q, debug=debug)
+    if(debug == True):
+        print(f"Error between computation of jacobian with skew theory and numerical derivatives method -> {err}")
+        print("------------------------------------------------------")
+    return err
+### For testing single point
+# q = [0, -np.pi/2, np.pi/2,   0,0,0]
+# calc_jacobian(q)
+
+### For testing the test dataset
+gen_data_FK = np.load('test_data_FK.npy')
+
+total_err = 0
+for i,q_data in enumerate(gen_data_FK):
+    q = q_data
+    total_err += calc_jacobian(q, debug=False)
+
+print("######################################################")
+print(f"Error between computation of jacobian with skew theory and numerical derivatives method for {len(gen_data_FK)} configurations-> \n Average Error: {total_err/len(gen_data_FK)} \t Total Error: {total_err}")
+print("------------------------------------------------------")
+
+
+# -------------------------- Singularities --------------------------
+# 1. Shoulder:
+print("Shoulder singularity:")
+q = [0, -1.6154540260077814, +0.04465769921288487, 0, np.pi/6, 0.0] # shoulder singularity (1st and 4th)
+robot.check_singularity(q)
+print("------------------------------------------------------")
+# Ts = []
+# for i in range(10):
+#     q = [i*np.pi/180, -np.pi/6, -1.990468423823186, 0,0, 0.0] # shoulder singularity
+#     T = robot.forward_kinematics(q, plot=False, debug=False, return_all=True)
+#     Ts.append(T)
+# robot.plot_robot_multi_frames(Ts)
+
+print("Shoulder (Alignment) singularity:")
+q = [0, -np.pi/6, -2.2829525304720546, 0, np.pi/6, 0.0] # shoulder(Alignment) singularity (1st and 6th)
+robot.check_singularity(q)
+print("------------------------------------------------------")
+# Ts = []
+# for i in range(10):
+#     q = [i*np.pi/180, -np.pi/6, -1.990468423823186, 0,0, 0.0] # shoulder singularity
+#     T = robot.forward_kinematics(q, plot=False, debug=False, return_all=True)
+#     Ts.append(T)
+# robot.plot_robot_multi_frames(Ts)
+
+# 2. Wrist:
+print("Wrist singularity:")
+q = [0, -np.pi/4, np.pi/3,   10*np.pi/180,0, 20*np.pi/180] # wrist
+robot.check_singularity(q)
+print("------------------------------------------------------")
+# T = robot.forward_kinematics(q, plot=True, debug=False)
+# Ts = []
+# for i in range(180):
+#     q = [0, -np.pi/4, 0,   i*np.pi/180,0, (i+10)*np.pi/180] # wrist
+#     T = robot.forward_kinematics(q, plot=False, debug=False, return_all=True)
+#     Ts.append(T)
+# robot.plot_robot_multi_frames(Ts)
+
+# 3. Elbow
+print("Elbow singularity:")
+q = [0, 0, 0,   0,-np.pi/2,0] # elbow
+robot.check_singularity(q)
+print("------------------------------------------------------")
+# T = robot.forward_kinematics(q, plot=True, debug=False)
+# Ts = []
+# for i in range(180):
+#     q = [0, 0, 0,   0,np.pi/4,0] # elbow
+#     T = robot.forward_kinematics(q, plot=False, debug=False, return_all=True)
+#     Ts.append(T)
+# robot.plot_robot_multi_frames(Ts)
+
+
+print("Elbow and wrist singularity:")
+q = [0, 0, 0,   0, 0, 0] # elbow & wrist
+robot.check_singularity(q)
+print("------------------------------------------------------")
+
+# q = [0, -np.pi/8, -np.pi/8,   -np.pi/8,-np.pi/2,-np.pi/10] # not singularity
+# q = [0, -np.pi/4, np.pi/3,   0,np.pi/8,0] # not singularity
+# robot.check_singularity(q)
---
+++ Midterm_preparation/Template/src/robot.py
@@ -0,0 +1,197 @@
+from utils import *
+import visualization as visual
+
+class KUKA_KR10_R1100_2:
+    def __init__(self, T_base=None, T_tool=None):
+        self.links_dimensions = KUKA_KR10_R1100_2_configs.get_links_dimensions()
+        self.joint_limits = KUKA_KR10_R1100_2_configs.get_joints_limits()
+        self.T_base = translation_x(0) if T_base is None else T_base
+        self.T_tool = translation_x(0) if T_tool is None else T_tool
+    
+    def print_frames(self, frames):
+        print(f"Note: Frame #{len(frames)-1} -> Tool")
+        for i, f in enumerate(frames):
+            print(f"Frame #{i}:")
+            if(i == len(frames) - 1):
+                print(f"--- Tool Frame ---")
+            else:
+                print(f"--- Joint #{i} Frame ---")
+            print_matrix(f)
+            print("-----------")
+
+
+    def print_angles(self, q):
+        for i, angle in enumerate(q):
+            print(f"Joint #{i}: {angle} rad ---> {angle*180/np.pi} degrees")
+
+    def plot_robot(self, T):
+        vis = visual.RobotVisualization_vpython(rate=10, scale=0.0002, radius={"link":0.007, "joint":0.008, "node":0.01, "axe":0.003})
+        frame = []
+        links = []
+        joints = []
+        node = get_position(T[-1])
+        for i in range(1,len(T)):
+            links.append((get_position(T[i-1]), get_position(T[i])))
+            joints.append(get_position(T[i]))
+        for i, l in enumerate(links):
+            # i == (shift_link-1)
+            if(i == 0): # because of the physical shift without link (if I was using DH it would be much easier)
+                p1 = l[0]
+                p2 = l[1]
+                # print(p2)
+                p1_1 = p1.copy()
+                p2_1 = p2.copy()
+                p2_2 = p2.copy()
+                p2_1[0] = 0.0
+                p1_2 = p2_1.copy()
+                frame.append(["link", p1_1, p2_1])
+                frame.append(["link", p1_2, p2_2])
+                continue
+            frame.append(["link", l[0], l[1]])
+        for j in joints:
+            frame.append(["joint", j])
+        frame.append(["node", node])
+        frame.append(["time", 0, 0])
+        # print(frame)
+        while True:
+            vis.render_frame(frame, axis=False)
+
+    def plot_robot_multi_frames(self, Ts):
+        while True:
+            for idx, T in enumerate(Ts):
+                # print(T)
+                vis = visual.RobotVisualization_vpython(rate=1, scale=0.0002, radius={"link":0.007, "joint":0.008, "node":0.01, "axe":0.003})
+                frame = []
+                links = []
+                joints = []
+                node = get_position(T[-1])
+                for i in range(1,len(T)):
+                    links.append((get_position(T[i-1]), get_position(T[i])))
+                    joints.append(get_position(T[i]))
+                for i, l in enumerate(links):
+                    # i == (shift_link-1)
+                    if(i == 0): # because of the physical shift without link (if I was using DH it would be much easier)
+                        p1 = l[0]
+                        p2 = l[1]
+                        # print(p2)
+                        p1_1 = p1.copy()
+                        p2_1 = p2.copy()
+                        p2_2 = p2.copy()
+                        p2_1[0] = 0.0
+                        p1_2 = p2_1.copy()
+                        frame.append(["link", p1_1, p2_1])
+                        frame.append(["link", p1_2, p2_2])
+                        continue
+                    frame.append(["link", l[0], l[1]])
+                for j in joints:
+                    frame.append(["joint", j])
+                frame.append(["node", node])
+                frame.append(["time", idx, 0])
+                # print(frame)
+                vis.render_frame(frame, axis=False)
+
+    def forward_kinematics(self, q, plot=True, debug=True, return_all=False):
+        from FK import FK
+        T = FK(q, T_base=self.T_base, T_tool=self.T_tool, return_frames=(plot or debug or return_all))
+        
+        if(debug == True):
+            self.print_frames(T)    # just print the result in a good way
+        if(plot == True):
+            self.plot_robot(T)    # plot the result
+        if(return_all == True):
+            return T
+        # We need only to return the end-effector
+        if(not(plot or debug) == True):
+            # print(T)    # only end_effector
+            return T
+        return T[-1]    # end_effector
+
+    def inverse_kinematics(self, T, m=-1, plot=True, debug=True):
+        from IK import IK
+
+        q, status = IK(T, T_base=self.T_base, T_tool=self.T_tool, m=m, debug=True)
+
+        if(plot == True):
+            self.plot_robot(T)    # plot the result
+        if(debug == True):
+            # self.print_frame(T)    # just print the result in a good way
+            self.print_angles(q)
+            print(status)
+
+        if(not debug == True):
+            # print(q)
+            return q
+
+        return q
+
+
+    def jacobian(self, q, method="skew"):
+        from Jacobian import Jacobian
+        jacobian = Jacobian(T_base=self.T_base, T_tool=self.T_tool)
+        if(method == "skew"):
+            return jacobian.calc_skew(q)
+        elif(method == "numerical"):
+            return jacobian.calc_numerical(q)
+    
+    def check_singularity(self, q, jacobian_method="numerical", singularity_method="rank", debug=True):
+        J = self.jacobian(q, method=jacobian_method)
+        singularity_flag = False
+        u, s, v = np.linalg.svd(J)
+        eps = 1e-15
+
+        if(singularity_method == "determinant"):
+            if(abs(np.linalg.det(J)) <= eps):
+                singularity_flag = True
+        
+        if(singularity_method == "SVD"):
+            if(min(s) <= eps):
+                singularity_flag = True
+        
+        if(singularity_method == "rank"):
+            if(np.linalg.matrix_rank(J) < 6):
+                singularity_flag = True
+
+        if(debug == True):
+            print("Checking Singularity ...")
+            print(f"Configuration (q): {q}")
+            # print(f"Jacobian (J): {J}")
+            print(f"SVD: s: {s}, minimum value: {min(s)}")
+            print(f"Determinant (det(J)): {np.linalg.det(J)}")
+            print(f"Rank (rank(J)): {np.linalg.matrix_rank(J)}")
+            print(f"Result: This configuration is {'a Singular' if singularity_flag == True else 'Not a Singular'}")
+        return singularity_flag
+class KUKA_KR10_R1100_2_configs:
+    @staticmethod
+    def get_links_dimensions():
+        return [400,25,560,25,515,90]
+
+    @staticmethod
+    def get_joints_limits():
+        deg = [(-170, 170), (-190, 45), (-120, 156), (-185, 185), (-120, 120), (-350, 350)]
+        rad = []
+        for (i,j) in deg:
+            rad.append(((i*np.pi/180), (j*np.pi/180)))
+        return rad
+
+if __name__ == "__main__":
+    robot = KUKA_KR10_R1100_2()
+    q = np.zeros((6,1))
+    skew = robot.jacobian(q, method="skew")
+    numerical = robot.jacobian(q, method="numerical")
+    print(skew)
+    print(numerical)
+    print(calc_error(numerical, skew))
+
+    # q = np.zeros((6,))
+    # # q[1] = -np.pi/4
+    # # q = [0, np.pi/4, 0,    1, 1, 0]
+    # T = robot.forward_kinematics(q, return_all=True)
+    # # # robot.print_frame(T)
+    # # q_calc = robot.inverse_kinematics(T)
+    # # T_calc = robot.forward_kinematics(q_calc, debug=False)
+
+    # # print(q, q_calc)
+    # # robot.print_frame(T)
+    # # robot.print_frame(T_calc)
+
+    # # print(f"Error using FK then IK then FK -> {calc_error(T, T_calc)}")
