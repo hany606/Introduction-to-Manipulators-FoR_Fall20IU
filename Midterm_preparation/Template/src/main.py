d27bf6ebcce11508ac55392ee1afa26d025a1f2b
hany606 <h.hamed.elanwar@gmail.com>
2020-11-16 18:21:25 +0300

    [Midterm Template] Add Expression & FK parsers
    - Convert from string to code expressions to be used later

--- Midterm_preparation/Template/src/main.py
+++ Midterm_preparation/Template/src/main.py
@@ -89,3 +89,37 @@ print("------------------------------------------------------")
 # q = [0, -np.pi/8, -np.pi/8,   -np.pi/8,-np.pi/2,-np.pi/10] # not singularity
 # q = [0, -np.pi/4, np.pi/3,   0,np.pi/8,0] # not singularity
 # robot.check_singularity(q)
+print("-------------------------------------------------------------------------")
+
+print("FK & IK Checking")
+
+### For testing the test dataset
+total_err = 0
+sp_debug = [None, None]
+num_issues = 0
+for i,q_data in enumerate(gen_data_FK):
+    q = q_data
+    T = robot.forward_kinematics(q, plot=False, debug=False)
+    # robot.print_frame(T)
+    q_calc = robot.inverse_kinematics(T, plot=False, debug=False)
+    T_calc = robot.forward_kinematics(q_calc, plot=False, debug=False)
+    # if(i == 4):
+    #     sp_debug[0] = q
+    #     sp_debug[1] = q_calc
+    # print(f"Original q: {q}\nComputed q: {q_calc}")
+    # print("------------------------------------------------------")
+    # robot.print_frame(T)
+    # print("------------------------------------------------------")
+    # robot.print_frame(T_calc)
+    # print("------------------------------------------------------")
+    total_err += calc_error(T, T_calc)
+    if(calc_error(T, T_calc) > 0.005):
+        num_issues += 1
+        print(f"Error in Configuration with index {i}\nOriginal q: {q}\nComputed q: {q_calc}")
+
+    # print(f"Error using FK then IK then FK (Test data #{i+1})-> {calc_error(T, T_calc)}")
+    # print("#########################################################################")
+print("######################################################")
+print(f"Number of configurations that has been calculated wrongly = {num_issues}")
+print(f"Error using FK then IK then FK for {len(gen_data_FK)} configurations-> \n Average Error: {total_err/len(gen_data_FK)} \t Total Error: {total_err}")
+print("------------------------------------------------------")
