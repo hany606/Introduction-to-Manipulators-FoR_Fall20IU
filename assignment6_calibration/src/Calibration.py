af79fccfa109d5d01fb43f0823f3d99ac1c836b2
hany606 <h.hamed.elanwar@gmail.com>
2020-12-15 06:59:20 +0300

    [Assignment6-Calibration] Fix bugs
    - Fix bugs related to the sizes of the numpy arrays

--- assignment6_calibration/src/Calibration.py
+++ assignment6_calibration/src/Calibration.py
@@ -3,6 +3,22 @@ import numpy as np
 import visualization as visual
 import time
 from Jacobian import Jacobian
+from utils import *
+from utils import translation_x as tx
+from utils import translation_y as ty
+from utils import translation_z as tz
+
+from utils import rotation_x as rx
+from utils import rotation_y as ry
+from utils import rotation_z as rz
+
+from utils import dtranslation_x as dtx
+from utils import dtranslation_y as dty
+from utils import dtranslation_z as dtz
+
+from utils import drotation_x as drx
+from utils import drotation_y as dry
+from utils import drotation_z as drz
 
 class Calibration:
     def __init__(self, robot,
@@ -22,6 +38,9 @@ class Calibration:
         self.jacobian = Jacobian(robot.robot_configs)
         
         self.pi_0 = np.zeros((self.num_unknown_parameters, 1))#? # TODO
+        
+        self.d = robot.robot_configs.get_links_dimensions()
+
         
         self.visualization_radius = {"node":0.003}
         self.visualization_scale = 0.1
@@ -91,33 +110,37 @@ class Calibration:
             vis.render_frame(frame, axis=True)
             # time.sleep(5)
             
-
+            
     # Use q, pi (error) in order to base and tool tranformations             
-    def _step1(self, q, pi):
-        T_robot = rz(q[0]) @ tx(self.d[1]+pi[0]) @ ty(pi[1]) @ rx(pi[2]) @ ry(q[1]+pi[3]) @ tx(pi[4]) @ rx(pi[5]) @ rz(pi[6]) @ ry(q[2]+pi[7]) @ tx(self.d[5]+pi[8]) @ tz(self.d[4]+pi[9]) @ rz(pi[10]) @ rx(q[3]+pi[11]) @ ty(pi[12]) @ tz(pi[13]) @ rz(pi[14]) @ ry(q[4] + pi[15]) @ tz(pi[16]) @ rz(pi[17]) @ rx(q[5])
-        p_robot_i = get_position(T_robot).reshape((3,1))
-        R_robot_i = get_rotation(T_robot).reshape((3,3))
-        # Calculate A matrix
-        A = np.empty((6+3*self.num_reference_points, 3))
-        for i in range(self.num_reference_points):
-            A[i*3:i*3+3, :3] = np.eye(3)
-            A[i*3:i*3+3, 3:6] = skew(p_robot_i).T
-            for j in range(self.num_reference_points):
-                if(j == i):
-                    A[i*3:i*3+3, 6+j*3:6+j*3+3] = R_robot_i
-
-                else:
-                    A[i*3:i*3+3, 6+j*3:6+j*3+3] = np.eye(3)
-                    
+    def _step1(self, pi):                    
         sum1 = np.zeros((6+3*self.num_reference_points,6+3*self.num_reference_points))
         sum2 = np.zeros((6+3*self.num_reference_points,1))
+        # sum
+        # print(sum2)
         for i in range(self.num_configs):
-            for j in range(self.num_samples): 
-                delta_pi = ?
+            for j in range(self.num_samples):
+                delta_p_i = np.empty((3*self.num_reference_points,1))
+                q = self.configruations[i, j].copy()
+                self.T_robot = rz(q[0]) @ tx(self.d[1]+pi[0]) @ ty(pi[1]) @ rx(pi[2]) @ ry(q[1]+pi[3]) @ tx(pi[4]) @ rx(pi[5]) @ rz(pi[6]) @ ry(q[2]+pi[7]) @ tx(self.d[5]+pi[8]) @ tz(self.d[4]+pi[9]) @ rz(pi[10]) @ rx(q[3]+pi[11]) @ ty(pi[12]) @ tz(pi[13]) @ rz(pi[14]) @ ry(q[4] + pi[15]) @ tz(pi[16]) @ rz(pi[17]) @ rx(q[5])
+                p_robot_i = get_position(self.T_robot).copy().reshape((3,1))
+                R_robot_i = get_rotation(self.T_robot).copy().reshape((3,3))
+                # Calculate A matrix
+                A = np.empty((3*self.num_reference_points, 6+3*self.num_reference_points))
+                for k in range(self.num_reference_points):
+                    A[k*3:k*3+3, :3] = np.eye(3)
+                    A[k*3:k*3+3, 3:6] = skew(p_robot_i).T
+                    for kk in range(self.num_reference_points):
+                        if(kk == k):
+                            A[k*3:k*3+3, 6+kk*3:6+kk*3+3] = R_robot_i
+
+                        else:
+                            A[k*3:k*3+3, 6+kk*3:6+kk*3+3] = np.zeros((3,3))
+                    delta_p_i_j = self.dataset[i,j,k].copy() - get_position(self.T_robot).copy()
+                    delta_p_i[k*3:k*3+3] = delta_p_i_j.reshape(3,1)
                 # Calculate the 1st summation
                 sum1 += A.T @ A
                 # Calulate the 2nd summation
-                sum2 += A.T @ delta_pi
+                sum2 += A.T @ delta_p_i
                 
         # Apply the formula and get p_base, phi_base, u_tool^1, ..., u_tool^num_reference_points
         tmp = np.linalg.inv(sum1) @ sum2  # shape: ((6+3n)x1)
@@ -143,44 +166,48 @@ class Calibration:
             for j in range(self.num_samples):
                 q = self.configruations[i, j].copy()
                 # Calculate identification Jacobian J_pi
-                jacobian_pi = self.jacobian.calc_identification_jacobian(T_base=T_base, T_tool=T_tool, q=q, pi=pi, pi_0=self.pi_0)
                 for k in range(self.num_reference_points):
-                    jacobian_pi_jp = jacobian_pi[:3].reshape((self.num_unknown_parameters,3))
-                    delta_pi = ?
+                    jacobian_pi = self.jacobian.calc_identification_jacobian(T_base=T_base, T_tool=T_tool[k], q=q, pi=pi, pi_0=self.pi_0)
+                    jacobian_pi_jp = jacobian_pi[:3].reshape((3, self.num_unknown_parameters))
+                    delta_p_i_j = (self.dataset[i,j,k].copy() - get_position(self.T_robot).copy()).reshape((3,1))
                     
                     # Calculate the 1st summation
                     sum1 += jacobian_pi_jp.T @ jacobian_pi_jp
                     # Calculate the 2nd summation
-                    sum2 += jacobian_pi_jp.T @ delta_p_i
+                    sum2 += jacobian_pi_jp.T @ delta_p_i_j
                     
         # Apply the formula and get delta_pi
         delta_pi = np.linalg.inv(sum1) @ sum2
         
         return delta_pi
     
-    def calibration(self, max_num_steps=1000, alpha=0.001, epsilon=1e-8):
+    def calibrate(self, max_num_steps=1000, alpha=0.001, epsilon=1e-8):
         steps = 0
         # define pi
         pi = self.pi_0
         while True:
             T_base, T_tool = self._step1(pi)
+            print("step1")
             delta_pi = self._step2(T_base, T_tool, pi)
+            print("step2")
             pi += alpha*delta_pi
             
             print(delta_pi)
             if(steps >= max_num_steps or np.all(np.abs(delta_pi) <= epsilon)):
                 break
-            
+            print("----------------")
             steps += 1
 
             
 if __name__ == "__main__":
-    calib = Calibration()
-
-    mat = calib.get_dataset_raw()
-    calib.visualize()
+    from robot import FANUC_R_2000i
+    robot = FANUC_R_2000i()
+    calib = Calibration(robot=robot)
+    calib.calibrate(max_num_steps=5)
+    # mat = calib.get_dataset_raw()
+    # calib.visualize()
+    
     # sample = calib.splitter()
     # print(sample[0] - mat["mA"][0], sample[1] - mat["mB"][0], sample[2] - mat["mC"][0])
     
-    print(len(mat["mA"]))
     
