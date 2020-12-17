e69d85296ad2b66c75305e197a254b1608b31596
hany606 <h.hamed.elanwar@gmail.com>
2020-12-17 10:31:34 +0300

    [Final Preparation] Add Euler Lagrange with the Jacobian method

---
+++ Final_preparation/src/main.py
@@ -0,0 +1,66 @@
+from EulerLagrangeDynamics import EulerLagrange
+from NewtonEulerDynamics import NewtonEuler
+import numpy as np
+from utils import *
+from visualization import RobotVisualization_vpython
+from math import cos, sin,exp
+
+
+
+EL_dyn = EulerLagrange()
+NE_dyn = NewtonEuler()
+dt = 0.0004
+simulation_times_steps = 25000#5000#25000
+
+
+q0 = np.array([-np.pi/2, np.pi/2]).reshape(2,1) # config1
+# q0 = np.array([-np.pi/2, 0]).reshape(2,1) # no motion config2
+# q0 = np.array([np.pi/2, 0]).reshape(2,1)    # no motion config3
+# q0 = np.array([np.pi/2, np.pi/4]).reshape(2,1)  # config4
+# q0 = np.array([0, 0]).reshape(2,1) #config5
+# print(q0)
+dq0 = np.array([0,0]).reshape(2,1)
+# dq0 = np.array([0.5,0]).reshape(2,1)
+ut = [np.array([0,0]).reshape(2,1) for i in range(simulation_times_steps)]
+# ut = [np.array([5*sin(i/500),0]).reshape(2,1) for i in range(simulation_times_steps)]
+# plot_u(ut, dt=dt)
+
+print(f"Computing the direct dynamics for simulation time: {simulation_times_steps*dt} seconds")
+qt, dqt, ddqt = EL_dyn.direct(q0, dq0, ut, dt=dt, debug=False)
+
+# plot_trajectory([qt, dqt, ddqt], dt=dt)
+# vis = RobotVisualization_vpython(rate=1000, scale=0.07, radius={"link":0.003, "joint":0.004, "node":0.004, "axe":0.003, "trajectory_trail": 0.0009})
+# while True:
+#     for i in range(len(qt)):
+#         q = qt[i].copy()
+#         frame = [
+#                 ['link', [0, 0, 0], [0.8*cos(q[0]), 0.8*sin(q[0]), 0.]], 
+#                 # ['link', [0., 0., 0.], [ 25.,   0., 400.]],
+#                 ['link', [0.8*cos(q[0]), 0.8*sin(q[0]), 0.], [0.8*cos(q[0]) + 0.8*cos(q[0]+q[1]), 0.8*sin(q[0]) + 0.8*sin(q[0]+q[1]), 0.]],
+#                 ['joint', [0, 0, 0]], 
+#                 ['joint', [0.8*cos(q[0]), 0.8*sin(q[0]), 0.]]]
+#         vis.render_frame(frame, axis=False)
+
+
+ut_ne_inverse = NE_dyn.inverse(qt, dqt, ddqt)
+ut_ne_inverse = np.array(ut_ne_inverse).reshape(len(ut_ne_inverse),2,1)
+ut_el_inverse = np.array(EL_dyn.inverse(qt, dqt, ddqt)).astype(np.float64)
+qt2, dqt2, ddqt2 = EL_dyn.direct(q0, dq0, ut_ne_inverse, dt=dt, debug=False)
+
+plot_u(ut_ne_inverse, dt=dt)
+vis = RobotVisualization_vpython(rate=1000, scale=0.07, radius={"link":0.003, "joint":0.004, "node":0.004, "axe":0.003, "trajectory_trail": 0.0009})
+while True:
+    for i in range(len(qt2)):
+        q = qt2[i].copy()
+        frame = [
+                ['link', [0, 0, 0], [0.8*cos(q[0]), 0.8*sin(q[0]), 0.]], 
+                # ['link', [0., 0., 0.], [ 25.,   0., 400.]],
+                ['link', [0.8*cos(q[0]), 0.8*sin(q[0]), 0.], [0.8*cos(q[0]) + 0.8*cos(q[0]+q[1]), 0.8*sin(q[0]) + 0.8*sin(q[0]+q[1]), 0.]],
+                ['joint', [0, 0, 0]], 
+                ['joint', [0.8*cos(q[0]), 0.8*sin(q[0]), 0.]]]
+        vis.render_frame(frame, axis=False)
+
+        # while True:
+            # vis.render_frame(frame, axis=False)
+
+
---
+++ Final_preparation/src/visualization.py
@@ -0,0 +1,109 @@
+import numpy as np
+import vpython as vp
+
+class RobotVisualization_vpython:
+    def __init__(self, rate=100, scale=1,radius={"link":0.005, "joint":0.006, "node":0.008, "axe":0.003, "trajectory_trail":0.004}, origin=[0.0, 0.0, 0.0], axe_length=0.2):
+        self.rate = rate
+        self.radius = radius
+        self.axe_color = vp.vector(1, 1, 1)
+        self.link_color = vp.vector(242/255, 92/255, 25/255)
+        self.joint_color = vp.vector(1,1,1)
+        self.node_color = vp.vector(0,1,0)
+        self.trail_color = vp.vector(1,0,0)
+        self.text_color = vp.vector(0,1,0)
+        self.origin = origin
+        self.axe_length = axe_length
+        self.axe_radius = 0.03*axe_length
+        self.time_color = vp.vector(0,1,0)
+        self.time_text_pos = vp.vector(-axe_length, axe_length,0)
+        self.scale = scale
+
+    def render_frame(self, frame_sub, axis=True):
+        if(axis):
+            frame = [["axe", self.origin, [1,0,0], self.axe_length],
+                     ["axe", self.origin, [0,1,0], self.axe_length],
+                     ["axe", self.origin, [0,0,1], self.axe_length]] + frame_sub
+        else:
+            frame = frame_sub
+        c = []
+        visible_idx = []
+        for obj in frame:
+            shape_type = obj[0]
+            if(shape_type == "joint"):
+                v = vp.vector(*obj[1])*self.scale
+                color = self.joint_color if len(obj) <= 2 else vp.vector(*obj[2])
+                c.append(vp.sphere(pos=v, radius=self.radius["joint"], color=color))
+                # c.append(vp.box(pos=v, length=self.radius["joint"]*2, height=self.radius["joint"]*2, width=self.radius["joint"]*2, color=color))
+
+            elif(shape_type == "node"):
+                v = vp.vector(*obj[1])*self.scale
+                color = self.node_color if len(obj) <= 2 else vp.vector(*obj[2])
+                c.append(vp.sphere(pos=v, radius=self.radius["node"], color=color))
+                # c.append(vp.box(pos=v, length=self.radius["node"]*2, height=self.radius["node"]*2, width=self.radius["node"]*2, color=color))
+            elif(shape_type == "trajectory_trail"):
+                visible_idx.append(len(c))
+                v = vp.vector(*obj[1])*self.scale
+                color = self.trail_color if len(obj) <= 2 else vp.vector(*obj[2])
+                c.append(vp.sphere(pos=v, radius=self.radius["trajectory_trail"], color=color))
+            elif(shape_type == "text_joint"):
+                text = str(obj[1])
+                v = vp.vector(*obj[2])*self.scale
+                c.append(vp.text(text=text, pos=v,color=self.text_color, height=0.03))
+            elif(shape_type == "time"):
+                # text= "Steps: {:}, Seconds: {:.3f}".format(obj[1], obj[2])
+                text= "Steps: {:}".format(obj[1], obj[2])
+                c.append(vp.label(text=text, pos=self.time_text_pos, align="right", color=self.time_color))
+                # c.append(vp.text(text=text, pos=self.time_text_pos, align="right", height=self.time_text_height, billboard=True, emissive=True, color=self.time_color))
+            else:   # link, axe
+                v1 = vp.vector(*obj[1])*self.scale
+                v2 = vp.vector(*obj[2])*self.scale
+
+                if shape_type == "link":
+                    r = self.radius["link"]   #0.011
+                    _color = self.link_color
+
+                elif shape_type == "axe":
+                    r = self.radius["axe"]
+                    _color = self.axe_color
+                    c.append(vp.arrow(pos=v1,axis=v2, length=obj[3], color=_color, shaftwidth=self.axe_radius))
+                    continue
+                c.append(vp.curve(pos=[v1, v2], color=_color, radius=r))
+        vp.rate(self.rate)
+        for i in range(len(frame)):
+            if(i in visible_idx):
+                continue
+            c[i].visible = False
+
+if __name__ == "__main__":
+    vis = RobotVisualization_vpython(rate=10, scale=0.0005)
+    # frame = []
+    
+    # frame = [["link",[0,0,0],[0.3,0.1,0.1]], ["joint",[0.2,0.2,0.2]], ["node", [0.4,0.4,0.4]]]
+    # frame += [["time", 0, 0]]
+    frame = [
+            ['link', [0, 0, 0], [0., 0., 0.]], 
+            # ['link', [0., 0., 0.], [ 25.,   0., 400.]],
+            ['link', [0., 0., 0.], [0,   0., 400.]],
+            ['link', [0., 0., 400.], [25,   0., 400.]],
+            ['link', [ 25.,   0., 400.], [585.,   0., 400.]], 
+            ['link', [585.,   0., 400.], [610.,   0., 400.]], 
+            ['link', [610.,   0., 400.], [1125.,    0.,  400.]], 
+            ['link', [1125.,    0.,  400.], [1125.,    0.,  400.]], 
+            ['link', [1125.,    0.,  400.], [1215.,    0.,  400.]], 
+            ['link', [1215.,    0.,  400.], [1215.,    0.,  400.]], 
+            ['joint', [0., 0., 0.]], 
+            ['joint', [ 25.,   0., 400.]], 
+            ['joint', [585.,   0., 400.]], 
+            ['joint', [610.,   0., 400.]], 
+            ['joint', [1125.,    0.,  400.]], 
+            ['joint', [1125.,    0.,  400.]], 
+            ['joint', [1215.,    0.,  400.]], 
+            ['joint', [1215.,    0.,  400.]], 
+            ['node', [1215.,    0.,  400.]], 
+            ]
+
+    while True:
+        # for i in range(500):
+        #     frame[-1][1] = i
+        vis.render_frame(frame)
+
